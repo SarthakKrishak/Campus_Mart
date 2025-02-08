226149c30ccf736055737b08e29cb2490222cc0d
Sarthak <sarthakkrishak1234@gmail.com>
2025-02-08 12:50:46 +0530

    product_listing page updation

--- frontend/src/Pages/ProductListing.jsx
+++ frontend/src/Pages/ProductListing.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Header from "../Components/Header";
 import axios from "axios";
+import { Images } from "lucide-react";
+import { ArrowUpFromLine } from "lucide-react";
 
 const ProductListing = () => {
     const [selectedCategory, setSelectedCategory] = React.useState("");
@@ -41,7 +43,8 @@ const ProductListing = () => {
         axios
             .post("/product/register", formData, {
                 headers: {
-                    "Content-Type": "multipart/form-data", ads
+                    "Content-Type": "multipart/form-data",
+                    ads,
                 },
             })
             .then((response) => {
@@ -57,7 +60,7 @@ const ProductListing = () => {
             <Header />
             <div className="flex">
                 {/* Left section */}
-                <div className="flex flex-col gap-5 w-1/3 h-full">
+                <div className="flex flex-col gap-2 w-1/3 h-full">
                     <div className="flex items-center pl-14 pt-5 gap-3 font-medium">
                         <img
                             width={30}
@@ -67,13 +70,18 @@ const ProductListing = () => {
                         />
                         <h1 className="text-2xl">Sell an Item</h1>
                     </div>
-                    <div className="h-[75vh] bg-gradient-to-tr from-[#534ff2] to-[#394ff1] flex flex-col justify-center items-center ml-14 mr-16 rounded-3xl text-white gap-4">
-                        <h1 className="text-lg">Product Images</h1>
+                    {/* Image upload */}
+                    <div className="h-[78vh] bg-gradient-to-tr from-[#534ff2] to-[#394ff1] flex flex-col justify-center items-center ml-14 mr-16 rounded-3xl text-white gap-4">
+                        <div className="flex flex-col justify-center items-center gap-1">
+                            <Images className="size-14" />
+                            <h1 className="text-lg">Product Images</h1>
+                        </div>
                         <button
-                            className="bg-white text-black px-10 py-2 rounded-lg text-lg"
+                            className="bg-white text-black py-2 px-6 rounded-lg text-lg flex justify-between gap-2 items-center"
                             onClick={() => document.getElementById("fileInput").click()} // Trigger the hidden input
                         >
                             Add Image
+                            <ArrowUpFromLine className="size-5" />
                         </button>
                         <input
                             type="file"
@@ -85,9 +93,13 @@ const ProductListing = () => {
                 </div>
 
                 {/* Right section */}
-                <div className="w-2/3 flex justify-between">
-                    <div className="pt-16">
-                        <form onSubmit={handleSubmit} className="flex flex-col">
+                <div className="w-2/3 flex mt-12">
+                    <form onSubmit={handleSubmit} className="flex">
+
+
+                           
+                        <div className="flex flex-col h-full w-1/2 pr-12">
+                            {/* Product name */}
                             <label htmlFor="productName" className="font-semibold">
                                 Product Name<span className="text-red-500">*</span>
                             </label>
@@ -100,69 +112,38 @@ const ProductListing = () => {
                                 value={productName}
                                 onChange={(e) => setProductName(e.target.value)}
                             />
-                            <div className="font-semibold mt-3 mb-2">
-                                Category<span className="text-red-500">*</span>
+                            {/* Category */}
+                            <div className="font-semibold mt-4 mb-2 flex items-center gap-2">
+                                <div>Category<span className="text-red-500">*</span></div>
+                                <h5 className="text-black/50 text-sm font-normal">
+                                    Select the appropriate category
+                                </h5>
                             </div>
-                            <div className="flex gap-3">
-                                <button
-                                    type="button"
-                                    onClick={() => handleCategorySelect("Electronics")}
-                                    className="bg-slate-200 p-2 rounded-md hover:bg-slate-300"
-                                >
-                                    Electronics
-                                </button>
-                                <button
-                                    type="button"
-                                    onClick={() => handleCategorySelect("Clothing")}
-                                    className="bg-slate-200 p-2 rounded-md hover:bg-slate-300"
-                                >
-                                    Clothing
-                                </button>
-                                <button
-                                    type="button"
-                                    onClick={() => handleCategorySelect("Daily Use")}
-                                    className="bg-slate-200 p-2 rounded-md hover:bg-slate-300"
-                                >
-                                    Daily Use
-                                </button>
-                                <button
-                                    type="button"
-                                    onClick={() => handleCategorySelect("Cycle")}
-                                    className="bg-slate-200 p-2 rounded-md hover:bg-slate-300"
-                                >
-                                    Cycle
-                                </button>
-                                <button
-                                    type="button"
-                                    onClick={() => handleCategorySelect("Others")}
-                                    className="bg-slate-200 p-2 rounded-md hover:bg-slate-300"
-                                >
-                                    Others
-                                </button>
-                            </div>
-                            <h5 className="text-gray-400 text-sm mt-2">
-                                Select the appropriate category
-                            </h5>
 
-                            <label htmlFor="productDesc" className="font-semibold mt-3">
-                                Description<span className="text-red-500">*</span>
-                            </label>
-                            <input
-                                required
-                                id="productDesc"
-                                type="text"
-                                placeholder="Enter product description"
-                                className="outline-none border border-slate-300 rounded-md px-2 py-7"
-                                value={productDesc}
-                                onChange={(e) => setProductDesc(e.target.value)}
-                            />
+                            <select
+                                name=""
+                                id="opts"
+                                className="bg-white shadow-[0px_4px_4px_0px_rgba(170,170,170,0.14)] border border-black/10 outline-none py-2 px-1 rounded-md"
+                                value={productCondition}
+                                onChange={(e) => setProductCondition(e.target.value)}
+                            >
+                                <option value="Electronics">Electronics</option>
+                                <option value="Books">Books</option>
+                                <option value="Daily Essential">Daily Essential</option>
+                                <option value="Cycles">Cycles</option>
+                                <option value="Mattress">Mattress</option>
+                            </select>
 
-                            <div className="flex mt-3 gap-4">
-                                <div className="border border-slate-300 flex flex-col justify-center items-center p-5 gap-2">
+                            {/* Condition and  date of purchase */}
+                            <div className="flex mt-5 gap-3">
+                                <div className="border border-black/10 flex flex-col justify-center items-center p-5 gap-2 rounded w-56">
+                                    <h1 className="font-semi">
+                                        Condition<span className="text-red-500">*</span>
+                                    </h1>
                                     <select
                                         name=""
                                         id="opts"
-                                        className="bg-slate-200 px-8 py-2"
+                                        className="bg-slate-200 px-12 py-2"
                                         value={productCondition}
                                         onChange={(e) => setProductCondition(e.target.value)}
                                     >
@@ -170,84 +151,108 @@ const ProductListing = () => {
                                         <option value="Good">Good</option>
                                         <option value="Poor">Poor</option>
                                     </select>
-                                    <h1 className="font-semi">
-                                        Condition<span className="text-red-500">*</span>
-                                    </h1>
+
                                 </div>
-                                <div className="border border-slate-300 flex flex-col justify-center items-center p-5 gap-2">
+                                <div className="border border-black/10 flex flex-col justify-center items-center p-5 gap-2 rounded w-60">
+                                    <h1 className="font-semi">
+                                        Date of Purchase<span className="text-red-500">*</span>
+                                    </h1>
                                     <input
                                         type="date"
                                         name=""
                                         id=""
                                         className="bg-slate-200 px-6 py-1"
                                     />
-                                    <h1 className="font-semi">
-                                        Date of Purchase<span className="text-red-500">*</span>
-                                    </h1>
+
                                 </div>
                             </div>
 
+                            {/* brand and model */}
                             <label htmlFor="productDesc" className="font-semibold mt-3">
-                                Pickup Address<span className="text-red-500">*</span>
+                               Brand & Model (if applicable)<span className="text-red-500">*</span>
                             </label>
                             <input
                                 required
                                 id="productAddress"
                                 type="text"
-                                placeholder="Enter pickup address"
+                                placeholder="Enter Brand or model name"
                                 className="outline-none border border-slate-300 rounded-md px-2 py-2 mt-2"
                                 value={productAddress}
                                 onChange={(e) => setProductAddress(e.target.value)}
                             />
-                        </form>
-                    </div>
 
-                    <div className="flex flex-col gap-4 ml-5 pl-16 justify-center">
-                        <div className="h-[38vh] w-[24.5vw] bg-slate-300 flex mt-16 rounded-md">
-                            Google Ads
+                            {/* Description */}
+                            <label htmlFor="productDesc" className="font-semibold mt-3">
+                                Description<span className="text-red-500">*</span>
+                            </label>
+                            <textarea
+                                required
+                                id="productDesc"
+                                placeholder="Add all the details about the product and provide enough details to trust your product."
+                                className="outline-none border border-slate-300 rounded-md px-2 py-2 h-36 w-full resize-none overflow-auto"
+                                value={productDesc}
+                                onChange={(e) => setProductDesc(e.target.value)}
+                            ></textarea>
+
+
+
+                            
+
+                            
                         </div>
-                        <div>
+
+                        <div className="flex flex-col gap-4 ml-5 pl-16 justify-center h-full w-1/2">
+                            <div className="h-[38vh] w-[24.5vw] bg-slate-300 flex mt-16 rounded-md">
+                                Google Ads
+                            </div>
                             <div>
-                                <h1 className="text-3xl font-bold">Price</h1>
-                                <div className="flex text-5xl pt-4 items-center gap-2">
-                                    <span className="text-slate-400 font-bold">₹</span>
-                                    <input
-                                        required
-                                        type="text"
-                                        name="productPrice"
-                                        id="productPrice"
-                                        min="1"
-                                        className="text-black font-extrabold border border-slate-300 flex outline-none rounded-md sm:w-2/3 px-2 py-2 focus:ring-2 focus:ring-[#364EF2] focus:border-[#364EF2] transition duration-200 ease-in-out"
-                                        placeholder="Enter price"
-                                        value={productPrice}
-                                        onChange={(e) => setProductPrice(e.target.value.replace(/[^0-9.]/g, ''))}
-                                        step="any"
-                                    />
-                                </div>
-                                <div className="flex gap-2 mt-3">
-                                    <input
-                                        type="checkbox"
-                                        id="terms"
-                                        checked={termsAccepted}
-                                        onChange={handleTermsChange}
-                                    />
-                                    <label className="select-none" htmlFor="terms">
-                                        I confirm that all the details are correct and genuine.
-                                        <span className="text-red-500">*</span>
-                                    </label>
+                                <div>
+                                    <h1 className="text-3xl font-bold">Price</h1>
+                                    <div className="flex text-5xl pt-4 items-center gap-2">
+                                        <span className="text-slate-400 font-bold">₹</span>
+                                        <input
+                                            required
+                                            type="text"
+                                            name="productPrice"
+                                            id="productPrice"
+                                            min="1"
+                                            className="text-black font-extrabold border border-slate-300 flex outline-none rounded-md sm:w-2/3 px-2 py-2 focus:ring-2 focus:ring-[#364EF2] focus:border-[#364EF2] transition duration-200 ease-in-out"
+                                            placeholder="Enter price"
+                                            value={productPrice}
+                                            onChange={(e) =>
+                                                setProductPrice(
+                                                    e.target.value.replace(/[^0-9.]/g, "")
+                                                )
+                                            }
+                                            step="any"
+                                        />
+                                    </div>
+                                    <div className="flex gap-2 mt-3">
+                                        <input
+                                            type="checkbox"
+                                            id="terms"
+                                            checked={termsAccepted}
+                                            onChange={handleTermsChange}
+                                        />
+                                        <label className="select-none" htmlFor="terms">
+                                            I confirm that all the details are correct and genuine.
+                                            <span className="text-red-500">*</span>
+                                        </label>
+                                    </div>
                                 </div>
                             </div>
+                            <div className="flex">
+                                <button
+                                    type="submit"
+                                    disabled={!termsAccepted}
+                                    className="bg-black text-white rounded-md py-2 w-[24.5vw]"
+                                >
+                                    Upload
+                                </button>
+                            </div>
                         </div>
-                        <div className="flex">
-                            <button
-                                type="submit"
-                                disabled={!termsAccepted}
-                                className="bg-black text-white rounded-md py-2 w-[24.5vw]"
-                            >
-                                Upload
-                            </button>
-                        </div>
-                    </div>
+
+                    </form>
                 </div>
             </div>
         </div>
