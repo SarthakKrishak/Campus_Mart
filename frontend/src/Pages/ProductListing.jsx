9b0595e74eafed5e820a87f186dae74993e07a8d
Sarthak <sarthakkrishak1234@gmail.com>
2025-05-20 20:11:47 +0530

    productListing

--- frontend/src/Pages/ProductListing.jsx
+++ frontend/src/Pages/ProductListing.jsx
@@ -6,6 +6,7 @@ import { IoImages } from "react-icons/io5";
 import { ArrowUpFromLine } from "lucide-react";
 import { MdChangeCircle } from "react-icons/md";
 import { FaRupeeSign } from "react-icons/fa";
+import Select from "react-select";
 
 const ProductListing = () => {
   const [selectedCategory, setSelectedCategory] = React.useState("");
@@ -58,11 +59,32 @@ const ProductListing = () => {
       });
   };
 
+  const options = [
+    { value: "Electronics", label: "Electronics" },
+    { value: "Books", label: "Books" },
+    { value: " Daily Essential", label: " Daily Essential" },
+    { value: "Cycles", label: "Cycles" },
+    { value: "Mattress", label: "Mattress" },
+    { value: "Others", label: "Others" },
+  ];
+
+  const customStyles = {
+    control: (provided) => ({
+      ...provided,
+      backgroundColor: "#1A1D20",
+      color: "#000",
+    }),
+    singleValue: (provided) => ({
+      ...provided,
+      color: "rgba(0, 0, 0, 0.5)",
+    }),
+  };
+
   return (
     <div className="overflow-hidden">
       <Header color={"#394ff1"} textColor={"white"} bagUrl={"whitebag.png"} />
       {/* start */}
-      <div className="flex flex-col dark:bg-[#131313]">
+      <div className="flex flex-col dark:bg-[#131313] pt-3 xl:pt-1">
         {/* top section */}
         <div className="flex flex-col lg:flex-row w-full lg:h-[78vh]">
           {/* Top left */}
@@ -73,19 +95,21 @@ const ProductListing = () => {
                 alt="image"
                 className="size-6 lg:h-8 lg:w-7"
               />
-              <h1 className="sm:text-[1.4vw] lg:text-[1.5vw] md:text-[2vw] font-poppins leading-normal font-medium dark:text-white">
+              <h1 className="sm:text-[1.4vw] lg:text-[1.5vw] text-[4vw] md:text-[2vw] font-poppins leading-normal font-medium dark:text-white">
                 List Your Product
               </h1>
             </div>
             {/* Image upload */}
-            <div className="lg:h-full lg:w-[24vw] bg-gradient-to-b from-[#394FF1] to-[#534FF2] shadow-[0px_4.410558223724365px_10px_0px_rgba(0,0,0,0.18)] flex flex-col lg:pl-7 items-start ml-5 lg:ml-14 lg:mr-16 mr-6 lg:rounded-2xl rounded-xl text-white gap-4 pt-14 pb-14 md:pt-20 md:pb-20 lg:pt-7 lg:pr-7 lg:gap-2 lg:pb-7">
+            <div className="lg:h-full lg:w-[24vw] bg-gradient-to-b from-[#394FF1] to-[#534FF2] shadow-[0px_4.410558223724365px_10px_0px_rgba(0,0,0,0.18)] flex flex-col lg:pl-7 items-start ml-5 lg:ml-14 lg:mr-16 mr-5 lg:rounded-2xl rounded-xl text-white gap-4 pt-6 xl:pl-7 pl-6 pb-6 md:pt-20 md:pb-20 lg:pt-7 lg:pr-7 lg:gap-2 lg:pb-7 pr-6">
               <div className="flex justify-center items-center gap-1 lg:gap-2">
-                <IoImages className="lg:size-6 size-8 md:size-9" />
-                <h1 className="lg:text-lg font-roboto">Product Images</h1>
+                <IoImages className="lg:size-6 size-5 md:size-9" />
+                <h1 className="lg:text-lg font-roboto text-lg">
+                  Product Images
+                </h1>
               </div>
-              <div className="w-full border border-violet-400 rounded-lg p-[0.4vw] flex items-center gap-2 lg:mt-2">
+              <div className="w-full border border-violet-400 rounded-lg p-[1.5vw] md:p-[0.4vw] flex items-center gap-2 lg:mt-2">
                 <button
-                  className="bg-white text-black lg:py-2 py-1 lg:px-4 px-4 lg:rounded-lg rounded-md lg:text-sm text-base flex justify-between gap-2 items-center font-roboto leading-loose"
+                  className="bg-white text-black lg:py-2 py-1 lg:px-4 px-5 lg:rounded-lg rounded-md lg:text-sm text-[3.2vw] flex justify-between gap-2 items-center font-roboto leading-loose"
                   onClick={() => document.getElementById("fileInput").click()}
                 >
                   Choose Files
@@ -97,16 +121,16 @@ const ProductListing = () => {
                   className="hidden"
                   onChange={handleImageChange}
                 />
-                <h1 className="lg:text-xs font-roboto leading-snug font-light">
+                <h1 className="lg:text-xs font-roboto leading-snug font-light text-[2.8vw] ml-3 md:ml-0">
                   Upload Images (up to 3)
                 </h1>
               </div>
-              <h1 className="font-roboto lg:text-[0.8vw] leading-snug">
+              <h1 className="font-roboto lg:text-[0.8vw] leading-snug text-xs">
                 Select one or more images. PNG, JPG, WEBP accepted.
               </h1>
-              <div className="border-dashed border border-violet-200 w-full h-full flex flex-col items-center justify-center rounded-2xl lg:mt-3">
+              <div className="border-dashed border border-violet-200 w-full h-full flex flex-col items-center justify-center rounded-2xl lg:mt-3 p-[20vw] md:p-0">
                 <IoImages className="lg:size-10 size-8 md:size-9" />
-                <h1 className="text-white text-sm md:text-base">
+                <h1 className="text-white text-xs md:text-base">
                   Previews will appear here.
                 </h1>
               </div>
@@ -114,8 +138,8 @@ const ProductListing = () => {
           </div>
 
           {/* top right */}
-          <div className="flex flex-col w-full lg:h-full lg:w-2/3 lg:ml-[-2.9vw] ">
-            <form className="flex flex-col lg:flex-row w-full gap-6 shadow-[0px_4px_12px_0px_rgba(0,0,0,0.10)] rounded-2xl lg:pt-5 pr-6 lg:pr-8 pl-6 lg:pl-8 pb-5 lg:mt-16 mt-12 dark:bg-[#1A1D20]">
+          <div className="flex flex-col md:w-full lg:h-full lg:w-2/3 lg:ml-[-2.9vw] ml-5 mr-5">
+            <form className="flex flex-col lg:flex-row w-full gap-6 shadow-[0px_4px_12px_0px_rgba(0,0,0,0.10)] rounded-2xl lg:pt-5 pr-6 lg:pr-8 pl-6 lg:pl-8 pb-5 lg:mt-16 md:mt-12 mt-6 dark:bg-[#1A1D20] pt-4">
               <div className="flex flex-col h-full lg:w-1/2">
                 {/* Product name */}
                 <label
@@ -145,7 +169,7 @@ const ProductListing = () => {
                   required
                   id="productDesc"
                   placeholder="Add all the details about the product and provide enough details to trust your product."
-                  className="outline-none border border-slate-300 rounded-md px-2 py-2 lg:h-[28.5vh] h-[24vh] w-full resize-none overflow-auto placeholder:text-sm font-roboto dark:bg-[#1A1D20] dark:border-[#515151] dark:placeholder:text-[#D7D7D7] dark:text-white"
+                  className="outline-none border border-slate-300 rounded-md px-2 py-2 lg:h-[28.5vh] h-[24vh] w-full resize-none overflow-auto placeholder:text-sm font-roboto dark:bg-[#1A1D20] dark:border-[#515151] dark:placeholder:text-[#D7D7D7] dark:text-white mt-1"
                   //   value={productDesc}
                   onChange={(e) => setProductDesc(e.target.value)}
                 ></textarea>
@@ -153,7 +177,7 @@ const ProductListing = () => {
 
               <div className="flex flex-col h-full lg:w-1/2">
                 {/* Category */}
-                <div className="font-semibold flex items-center gap-2 text-sm lg:text-base">
+                <div className="font-medium flex items-center gap-2 text-sm lg:text-base">
                   <div className="text-[#534ff2] font-roboto dark:text-white">
                     Category<span className="text-red-500">*</span>
                   </div>
@@ -162,58 +186,19 @@ const ProductListing = () => {
                   </h5>
                 </div>
 
-                <select
-                  name=""
-                  id="opts"
-                  className="bg-white shadow-[0px_4px_4px_0px_rgba(170,170,170,0.14)] border border-black/10 outline-none py-2 px-1 rounded-md font-roboto dark:bg-[#131313] dark:shadow-none dark:text-[#848484]"
-                  value={productCondition}
-                  onChange={(e) => setProductCondition(e.target.value)}
-                >
-                  <option className="text-sm lg:text-base font-roboto" value="">
-                    Choose the Category
-                  </option>
-                  <option
-                    className="text-sm lg:text-base font-roboto"
-                    value="Electronics"
-                  >
-                    Electronics
-                  </option>
-                  <option
-                    className="text-sm lg:text-base font-roboto"
-                    value="Books"
-                  >
-                    Books
-                  </option>
-                  <option
-                    className="text-sm lg:text-base font-roboto"
-                    value="Daily Essential"
-                  >
-                    Daily Essential
-                  </option>
-                  <option
-                    className="text-sm lg:text-base font-roboto"
-                    value="Cycles"
-                  >
-                    Cycles
-                  </option>
-                  <option
-                    className="text-sm lg:text-base font-roboto"
-                    value="Mattress"
-                  >
-                    Mattress
-                  </option>
-                  <option
-                    className="text-sm lg:text-base font-roboto"
-                    value="Other"
-                  >
-                    Other
-                  </option>
-                </select>
+                <Select
+                  options={options}
+                  className="mt-1"
+                  classNames={{
+                    control: () => "bg-[#1A1D20] text-black/50 dark:bg-red",
+                    singleValue: () => "text-black/50",
+                  }}
+                />
 
                 {/* brand and model */}
                 <label
                   htmlFor="productDesc"
-                  className="font-semibold mt-4 text-[#534ff2] text-sm lg:text-base font-roboto dark:text-white"
+                  className="font-medium mt-4 text-[#534ff2] text-sm lg:text-base font-roboto dark:text-white"
                 >
                   Brand & Model (if applicable)
                   <span className="text-red-500">*</span>
@@ -223,13 +208,13 @@ const ProductListing = () => {
                   id="productAddress"
                   type="text"
                   placeholder="Enter Brand or model name"
-                  className="outline-none border border-slate-300 rounded-md px-2 py-2 placeholder:text-sm font-roboto dark:bg-[#131313] dark:placeholder:text-[#848484] dark:text-white dark:border-0"
+                  className="outline-none border border-slate-300 rounded-md px-2 py-2 placeholder:text-sm font-roboto dark:bg-[#131313] dark:placeholder:text-[#848484] dark:text-white dark:border-0 mt-1"
                 />
 
                 {/* Color */}
                 <label
                   htmlFor="productDesc"
-                  className="font-semibold mt-3 text-[#534ff2] text-sm lg:text-base font-roboto dark:text-white"
+                  className="font-medium mt-3 text-[#534ff2] text-sm lg:text-base font-roboto dark:text-white"
                 >
                   Color<span className="text-red-500">*</span>
                 </label>
@@ -238,18 +223,18 @@ const ProductListing = () => {
                   id="productAddress"
                   type="text"
                   placeholder="Product Color"
-                  className="outline-none border border-slate-300 rounded-md px-2 py-2 placeholder:text-sm font-roboto dark:bg-[#131313] dark:text-white dark:placeholder:text-[#848484] dark:border-0"
+                  className="outline-none border border-slate-300 rounded-md px-2 py-2 placeholder:text-sm font-roboto dark:bg-[#131313] dark:text-white dark:placeholder:text-[#848484] dark:border-0 mt-1"
                 />
                 {/* Original Price */}
                 <label
                   htmlFor="productDesc"
-                  className="font-semibold mt-3 text-[#534ff2] text-sm lg:text-base font-roboto dark:text-white"
+                  className="font-medium mt-3 text-[#534ff2] text-sm lg:text-base font-roboto dark:text-white"
                 >
                   Original Price<span className="text-red-500">*</span>
                 </label>
-                <div className="flex outline-none lg:px-2 py-2 lg:ml-[-0.5vw] lg:w-[31.9vw] rounded-md mt-[-1.1vh] font-roboto">
+                <div className="flex outline-none lg:px-2 py-2 lg:ml-[-0.5vw] lg:w-[31.9vw] rounded-md md:mt-[-1.1vh] font-roboto mt-[-0.4vh]">
                   <div className="bg-neutral-100 outline outline-1 outline-neutral-300 px-4 rounded-s-md flex justify-center items-center font-roboto dark:bg-[#2A2A2A] dark:text-white dark:outline-[#515151]">
-                    $
+                    <FaRupeeSign className="size-3 xl:size-4 text-neutral-600" />
                   </div>
                   <input
                     required
@@ -264,11 +249,11 @@ const ProductListing = () => {
 
             {/* Condition and  date of purchase */}
             <div className="flex flex-col md:flex-row justify-between items-center gap-2 lg:mt-4 mt-7">
-              <div className="flex lg:gap-2 font-roboto">
+              <div className="flex lg:gap-2 font-roboto gap-2">
                 <div className="shadow-[0px_4px_6px_0px_rgba(111,111,111,0.12)] border-2 border-black/10 flex flex-col justify-center items-center lg:p-5 gap-2 rounded-lg lg:w-60 w-48 p-3 dark:bg-[#1A1D20] dark:text-[#DEDEDE]">
                   <h1 className="font-semi text-sm lg:text-base">
                     Condition<span className="text-red-500">*</span>
-                  </h1> 
+                  </h1>
                   <select
                     name=""
                     id="opts"
@@ -303,7 +288,7 @@ const ProductListing = () => {
                 </div>
               </div>
 
-              <div className="flex lg:gap-2 font-roboto">
+              <div className="flex lg:gap-2 font-roboto gap-2">
                 <div className="shadow-[0px_4px_6px_0px_rgba(111,111,111,0.12)] border-2 border-black/10 flex flex-col justify-center items-center lg:p-5 gap-2 rounded-lg lg:w-60 w-48 p-3 dark:bg-[#1A1D20] dark:text-[#DEDEDE]">
                   <h1 className="font-semi text-sm lg:text-base">
                     Is the price negotiable
@@ -366,8 +351,10 @@ const ProductListing = () => {
                 <h1 className="font-semibold mt-3 text-sm lg:text-base dark:text-[#D7D7D7]">
                   Pickup Address
                 </h1>
-                <button className="flex mt-3 justify-center items-center gap-1 text-sm lg:text-base">
-                  <h1 className="font-semibold text-[#646464] dark:text-[#D7D7D7]">Change</h1>
+                <button className="flex mt-3 justify-center items-center gap-1 text-xs md:text-sm lg:text-base">
+                  <h1 className="font-semibold text-[#646464] dark:text-[#D7D7D7]">
+                    Change
+                  </h1>
                   <MdChangeCircle className="text-blue-500 lg:size-8 size-6" />
                 </button>
               </div>
@@ -390,7 +377,7 @@ const ProductListing = () => {
             </div>
 
             {/* payment method */}
-            <div className="flex flex-col lg:w-1/3 lg:pl-8">
+            <div className="flex flex-col lg:w-1/3 lg:pl-8 mt-1">
               <label
                 htmlFor="payment"
                 className="font-semibold mt-3 text-sm lg:text-base md:mt-5 dark:text-[#F1F1F1]"
@@ -453,16 +440,18 @@ const ProductListing = () => {
             <div className="flex flex-col h-full lg:w-1/3 lg:mt-3 mt-5 md:mt-6">
               <div>
                 <div>
-                  <h1 className="lg:text-lg text-base font-bold dark:text-[#F1F1F1]">Price</h1>
+                  <h1 className="lg:text-lg text-base font-bold dark:text-[#F1F1F1]">
+                    Price
+                  </h1>
                   <div className="flex lg:text-[2.5vw] text-[6vw] items-center md:text-[4vw]">
-                    <FaRupeeSign className="text-stone-300 lg:text-[2vw] text-[5vw] md:text-[3vw] dark:text-[#C6C6C6] xl:text-[3vw]" />
+                    <FaRupeeSign className="text-stone-300 lg:text-[2vw] text-[5vw] md:text-[3vw] dark:text-[#C6C6C6] xl:text-[2.4vw]" />
                     <input
                       required
                       type="text"
                       name="productPrice"
                       id="productPrice"
                       min="1"
-                      className="text-black flex outline-none rounded-md sm:w-2/3 px-2 py-2 font-tiltWarp dark:bg-[#131313] dark:text-white"
+                      className="text-black flex outline-none rounded-md w-[80vw] sm:w-2/3 px-2 py-2 font-tiltWarp dark:bg-[#131313] dark:text-white"
                       placeholder="Enter price"
                       value={productPrice}
                       onChange={(e) =>
@@ -471,7 +460,7 @@ const ProductListing = () => {
                       step="any"
                     />
                   </div>
-                  <div className="flex gap-2 mt-3 dark:text-[#F1F1F1]">
+                  <div className="flex gap-2 md:mt-3 mt-4 dark:text-[#F1F1F1]">
                     <input
                       type="checkbox"
                       id="terms"
@@ -480,7 +469,7 @@ const ProductListing = () => {
                       className="lg:w-4 w-3"
                     />
                     <label
-                      className="select-none hover:cursor-pointer text-sm lg:text-base md:text-sm font-poppins xl:text-sm"
+                      className="select-none hover:cursor-pointer text-xs lg:text-base md:text-sm font-poppins xl:text-sm"
                       htmlFor="terms"
                     >
                       I confirm that all the details are correct and genuine.
@@ -489,11 +478,11 @@ const ProductListing = () => {
                   </div>
                 </div>
               </div>
-              <div className="flex mt-3">
+              <div className="flex md:mt-3 mt-5">
                 <button
                   type="submit"
                   disabled={!termsAccepted}
-                  className="bg-stone-900 text-white rounded-md md:py-3 py-2 lg:w-[26vw] w-[36vw] font-medium text-xs lg:text-base md:w-[26vw] dark:bg-[#F1F1F1] dark:text-[#1A1D20]"
+                  className="bg-stone-900 text-white rounded-md md:py-3 py-3 lg:w-[26vw] w-[85vw] font-medium text-sm lg:text-base md:w-[26vw] dark:bg-[#F1F1F1] dark:text-[#1A1D20]"
                 >
                   List my Product
                 </button>
